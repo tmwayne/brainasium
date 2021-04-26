d529632eb796042a7957170219a277be35745c23
Tyler Wayne <tylerwayne3@gmail.com>
2021-04-26 00:21:00 -0700

    add config reader

--- src/main.c
+++ src/main.c
@@ -7,15 +7,26 @@
 //
 
 #include <dict.h> // dict_T, dict_new, dict_free
+#include <configparse.h> // configparse
 #include "registry.h" // load_plugins
 #include "game.h" // game_T, game_init
 
+#define DEFAULT_USER_RC_PATH "/home/tyler/.config/gymrc"
+#define DEFAULT_GAMES_DIR "/home/tyler/.local/lib/gym/games"
+
+static void set_defaults(dict_T);
+
 int main(int argc, char** argv) {
 
-  char *plugin_dir = "/home/tyler/.local/lib/gym/games";
+  // Load configurations
+  dict_T configs = dict_new();
+  set_defaults(configs);
+
+  FILE *userrc = fopen(DEFAULT_USER_RC_PATH, "r");
+  if (userrc) configparse(configs, userrc);
 
   dict_T registry = dict_new();
-  load_plugins(registry, plugin_dir);
+  load_plugins(registry, dict_get(configs, "games_dir"));
 
   game_T game = game_init(registry, "sample");
   dict_free(&registry, (void (*)(void *)) entry_free);
@@ -26,3 +37,8 @@ int main(int argc, char** argv) {
 
 }
 
+static void set_defaults(dict_T configs) {
+  
+  dict_set(configs, "games_dir", DEFAULT_GAMES_DIR);
+
+}
